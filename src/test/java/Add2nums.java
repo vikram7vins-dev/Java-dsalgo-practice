c353ece80753c5c7768cef81bd6d1b832e41042d
vikram-dev <vikram7vins.dev@gmail.com>
2024-03-19 18:46:32 +0530

    add program

---
+++ src/test/java/Add2nums.java
@@ -0,0 +1,14 @@
+
+public class Add2nums {
+
+	public static int add(int a,int b) {
+		int c = a+b;
+		return c;
+	}
+	public static void main(String[] args) {
+		
+				
+		System.out.println("Thes sum of a and b is " +add(5, 8));
+	}
+
+}
